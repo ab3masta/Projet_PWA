17ca2fe69d1e9a18b5c810e4fbc760ff4a8d997b
AbdouAbarchiAboubacar <abdouabarchia@gmail.com>
2019-11-24 23:12:22 +0100

    make it better

--- pwa_project/views.py
+++ pwa_project/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import Http404
-def handler404(request,exception):
-    return render(request,'404.html',{'error': exception},status=404)
+# from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.http import Http404
+# def handler404(request,exception):
+#     return render(request,'404.html',{'error': exception},status=404)
            
-def handler500(request,exception=None):
-    return render(request,'500.html',{},status=500)    
+# def handler500(request,exception=None):
+#     return render(request,'500.html',{},status=500)    
