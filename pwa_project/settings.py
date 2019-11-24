9a41784a207b4920d1e163c9dd7dcf00b9965a9d
AbdouAbarchiAboubacar <abdouabarchia@gmail.com>
2019-11-24 23:29:17 +0100

    make it better

--- pwa_project/settings.py
+++ pwa_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'exemple.apps.BlogConfig',
+    'exemple.apps.ExempleConfig',
 ]
 
 MIDDLEWARE = [
